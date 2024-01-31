ecde20867162a8863a237915b4d89563bb8c6ab6
Grinevich Artem <grinevitch.artemy@yandex.ru>
2024-01-31 21:58:12 +0300

    сделать блок с ценами плавно появляющимеся друг за другом

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,12 @@
-import "./index.css";
+import './index.css';
+
+window.addEventListener('load', () => {
+    console.log(4);
+  const price = document.querySelectorAll('.price');
+  let time = 0;
+  price.forEach((elem) => {
+    setTimeout(() => elem.classList.add('price-visibil'), Number(`${time}000`));
+    time += 1;
+  });
+});
+
