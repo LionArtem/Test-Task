223fa22cfa99f1672d7af856d9516d30851e88a6
Grinevich Artem <grinevitch.artemy@yandex.ru>
2024-02-01 22:04:42 +0300

    изменить по клику значок валюты

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import './index.css';
+const currency = document.querySelectorAll('.dollar');
+const price = document.querySelectorAll('.number');
+const courseDollar = 91;
+const coursEuro = 98;
+const coursRuble = 1;
 
 window.addEventListener('load', () => {
-    console.log(4);
   const price = document.querySelectorAll('.price');
   let time = 0;
   price.forEach((elem) => {
@@ -10,3 +14,51 @@ window.addEventListener('load', () => {
   });
 });
 
+const arrCurrency = ['$', '₽', '€'];
+
+const changeCurrency = (evt) => {
+  const currentCurrency = evt.target.textContent;
+
+  const indexCurrency = arrCurrency.indexOf(currentCurrency);
+  if (indexCurrency != -1) {
+    arrCurrency.splice(indexCurrency, 1);
+    arrCurrency.push(currentCurrency);
+    currency.forEach((elem) => {
+      elem.textContent = arrCurrency[0];
+    });
+    price.forEach((elem) => {
+      let newPrice = 0;
+      switch (currentCurrency) {
+        case '$':
+          if (courseDollar > coursRuble) {
+            newPrice = courseDollar * elem.textContent;
+          } else {
+            newPrice = coursRuble / elem.textContent;
+          }
+          break;
+        case '₽':
+          if (coursEuro > courseDollar) {
+            newPrice = elem.textContent / courseDollar;
+          } else {
+            newPrice = courseDollar * elem.textContent;
+          }
+          break;
+        case '€':
+          if (coursEuro > coursRuble) {
+            newPrice = elem.textContent / coursEuro;
+          } else {
+            newPrice = coursEuro * elem.textContent;
+          }
+          break;
+        default:
+          newPrice = 0;
+          break;
+      }
+      elem.textContent = Math.floor(newPrice);
+    });
+  }
+};
+
+currency.forEach((elem) => {
+  elem.addEventListener('click', (evt) => changeCurrency(evt));
+});
